9304acb4f8f18a650a29b97ea01734dadb3c8db5
José André Paredes Quispe <90292121+Jerbo03@users.noreply.github.com>
2022-06-12 17:35:48 -0500

    Update Exercise2.java

--- src/main/java/com/example/project/Exercise2.java
+++ src/main/java/com/example/project/Exercise2.java
@@ -14,7 +14,19 @@ public class Exercise2 {
 
     public boolean existenDuplicados(String str) {
         MyStack<Character> stack = new LinkedListStack<>();
-        
-        return false;
+        int estado = 0; // 0 = bien, 1 = "((...", 2 = "((...))" => false
+        for (int i = 0; i < s.length(); i++) {
+            if (estado == 0)
+                if (stack.top().equals('(') == 0 && str.charAt(i) == '(')
+                    estado = 1;
+            if (estado == 1 && str.charAt(i) == ')') {
+                if (stack.top().equals(')') == 0)
+                    return false;
+                else
+                    estado = 0;
+            }
+            stack.push(str.charAt(i));
+        }
+        return true;
     }
 }
