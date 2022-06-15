41606a51600497d6727db623a69cb4c91cd58a87
José André Paredes Quispe <90292121+Jerbo03@users.noreply.github.com>
2022-06-14 21:54:23 -0500

    Nueva idea

--- src/main/java/com/example/project/Exercise2.java
+++ src/main/java/com/example/project/Exercise2.java
@@ -14,19 +14,14 @@ public class Exercise2 {
 
     public boolean existenDuplicados(String str) {
         MyStack<Character> stack = new LinkedListStack<>();
-        int estado = 0; // 0 = bien, 1 = "((...", 2 = "((...)", 3= false
-        stack.push(str.charAt(0));
-        for (int i = 1; i < str.length(); i++) {
-            if (estado == 2) {
-                if (str.charAt(i) == ')') return true;
-                else estado = 0;
+        for (int i = 0; i < str.length(); i++) { // Itineramos por el array
+            if (str.charAt(i) == ")") { // Si encontramos un parentesis de cierre
+                char last = stack.pop(); // Eliminamos el contenido que encierra
+                if (last == "(") return True; // Solo si no encierra ningun contenido, es porque su contenido ya pertenecia a otro par de parentesis
+                while (last != "(") last = stack.pop(); // Eliminamos el contenido que encierra
+                continue; // Y no agregamos nada
             }
-            if (estado == 1 && str.charAt(i) == ')')
-                estado = 2;
-            if (estado == 0)
-                if (stack.top().equals('(') && str.charAt(i) == '(')
-                    estado = 1;
-            stack.push(str.charAt(i));
+            stack.push(str.charAt(i)); // Si no es un parentesis de cierre agregamos el caracter
         }
         return false;
     }
