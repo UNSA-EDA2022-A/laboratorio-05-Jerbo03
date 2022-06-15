418d936c8091e4276bd9a710f115699e35570593
José André Paredes Quispe <90292121+Jerbo03@users.noreply.github.com>
2022-06-14 21:18:10 -0500

    Comentarios

--- src/main/java/com/example/project/Exercise1.java
+++ src/main/java/com/example/project/Exercise1.java
@@ -16,20 +16,20 @@ public class Exercise1 {
     public boolean esBalanceado(String s) {
         MyStack<Character> stack = new LinkedListStack<>();
         if (s.length() % 2 != 0) return false; // Debe existir un numero par de caracteres
-        for (int i = 0; i < s.length(); i++) {
+        for (int i = 0; i < s.length(); i++) { // Se itinera por todo el array
             if (s.charAt(i) == '(' || s.charAt(i) == '[' || s.charAt(i) == '{') {
-                stack.push(s.charAt(i));
+                stack.push(s.charAt(i));// Copiando los caracteres de abertura de agrupacion
             } else {
-                char top = stack.pop();
+                char top = stack.pop();// Si el caracter es de cierre, significa que primero debe haber uno de abertura
                 if(!(
                     (s.charAt(i) == ')' && top == '(') ||
                     (s.charAt(i) == '}' && top == '{') ||
                     (s.charAt(i) == ']' && top == '[')
                     )) {
-                    return false;
+                    return false; // Si no, el caracter esta de mas
                 }
             }
         }
-        return true;
+        return (stack.size() == 0); // Al final, comprobamos que todos los caracteres de abertura se hallan cerrado
     }
 }
