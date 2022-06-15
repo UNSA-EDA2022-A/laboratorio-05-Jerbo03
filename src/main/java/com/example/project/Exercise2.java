ed7e6c3d49683bf760ebc875988a44e2ae4f8ccc
José André Paredes Quispe <90292121+Jerbo03@users.noreply.github.com>
2022-06-14 21:55:39 -0500

    Corrección de comillas

--- src/main/java/com/example/project/Exercise2.java
+++ src/main/java/com/example/project/Exercise2.java
@@ -15,10 +15,10 @@ public class Exercise2 {
     public boolean existenDuplicados(String str) {
         MyStack<Character> stack = new LinkedListStack<>();
         for (int i = 0; i < str.length(); i++) { // Itineramos por el array
-            if (str.charAt(i) == ")") { // Si encontramos un parentesis de cierre
+            if (str.charAt(i) == ')') { // Si encontramos un parentesis de cierre
                 char last = stack.pop(); // Eliminamos el contenido que encierra
-                if (last == "(") return True; // Solo si no encierra ningun contenido, es porque su contenido ya pertenecia a otro par de parentesis
-                while (last != "(") last = stack.pop(); // Eliminamos el contenido que encierra
+                if (last == '(') return True; // Solo si no encierra ningun contenido, es porque su contenido ya pertenecia a otro par de parentesis
+                while (last != '(') last = stack.pop(); // Eliminamos el contenido que encierra
                 continue; // Y no agregamos nada
             }
             stack.push(str.charAt(i)); // Si no es un parentesis de cierre agregamos el caracter
